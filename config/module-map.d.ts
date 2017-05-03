14e50ddbcc87f2caca7170dc5b6163e24cd74b83
Oleg G.Kapranov <oleg@vertiopia.com>
2017-05-03 21:03:58 +0300

    added new ui

--- config/module-map.d.ts
+++ config/module-map.d.ts
@@ -1,8 +1,3 @@
-/**
- * This is just a placeholder file to keep TypeScript aware editors happy. At build time,
- * it will be replaced with a complete map of resolvable module paths => rolled up contents.
- */
-
 export interface Dict<T> {
   [index: string]: T;
 }
---
+++ src/ui/components/server-uptime/component.ts
@@ -0,0 +1,27 @@
+import Component, { tracked } from "@glimmer/component";
+
+export default class ServerUptime extends Component {
+  args: { days: any[] };
+
+  @tracked('args')
+  get upDays() {
+    return this.args.days.reduce((upDays, day) => {
+      return upDays += (day.up ? 1 : 0);
+    }, 0);
+  }
+
+  @tracked('args')
+  get streak() {
+    let [max] = this.args.days.reduce(([max, streak], day) => {
+      if (day.up && streak + 1 > max) {
+        return [streak + 1, streak + 1];
+      } else if (day.up) {
+        return [max, streak + 1];
+      } else {
+        return [max, 0];
+      }
+    }, [0, 0]);
+
+    return max;
+  }
+}
